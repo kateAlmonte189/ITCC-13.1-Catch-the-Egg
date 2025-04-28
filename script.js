e0efa1c2cdc5180b12b87613fa7c033f0447ff7d
SandyLMCD <officialsandy.ss@gmail.com>
2025-04-28 09:55:44 +0800

    name entry scene

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+  const playerName = document.getElementById("playerName").value.trim();
+  if (playerName) {
+    // Store the player name in localStorage to use it in the game
+    localStorage.setItem("playerName", playerName);
+    // Redirect to the game page
+    window.location.href = "/game.html";
+  } else {
+    alert("Please enter your name to continue!");
+  }
+}
