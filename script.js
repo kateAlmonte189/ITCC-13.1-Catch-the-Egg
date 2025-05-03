3e80c5dc35621044090ea43467bad15c796224d2
SandyLMCD <officialsandy.ss@gmail.com>
2025-05-03 09:16:40 +0800

    added game functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function startGame() {
+function goToGameScene() {
   const playerName = document.getElementById("playerName").value.trim();
   if (playerName) {
     // Store the player name in localStorage to use it in the game
@@ -25,193 +25,102 @@ function tryAgain() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Select DOM elements
+const basketImage = new Image();
+basketImage.src = "/assets/basket_noBG.png";
 
-const gameContainer = document.querySelector(".game-container");
-const scoreElement = document.querySelector(".score");
+const eggImage = new Image();
+eggImage.src = "/assets/egg_noBG.png";
+
+const gameContainer = document.querySelector("game-container");
+const scoreElement = document.getElementById("score");
 const livesContainer = document.querySelector(".lives-container");
 
-const basket = document.createElement("img");
+// Create basket element
+const basket = document.createElement("div");
 basket.classList.add("basket");
-basket.src = "/assets/basket_noBG.png";
 gameContainer.appendChild(basket);
 
-let basketX = 200;
-let basketY = 400;
+// Set initial basket position
+let basketPosition = 200;
+basket.style.left = `${basketPosition}px`;
 
+// Game variables
 let score = 0;
 let lives = 3;
 let gameInterval;
 
+// Handle keyboard input
 function handleKeyDown(event) {
-  const ket = event.key;
-  if (key === "ArrowLeft" && basketX > 0) {
-    basketX -= 20;
-  } else if (
-    key === "ArrowRight" &&
-    basketX < gameContainer.clientWidth - 100
-  ) {
-    basketX += 20;
+  const key = event.key;
+  if (key === "ArrowLeft") {
+    basketPosition -= 20;
+    if (basketPosition < 0) basketPosition = 0;
+  } else if (key === "ArrowRight") {
+    basketPosition += 20;
+    if (basketPosition > gameContainer.offsetWidth - basket.offsetWidth) {
+      basketPosition = gameContainer.offsetWidth - basket.offsetWidth;
+    }
   }
-  basket.style.left = `${basketX}px`;
+  basket.style.left = `${basketPosition}px`;
 }
 
 document.addEventListener("keydown", handleKeyDown);
 
+// Create egg element
 function createEgg() {
-  const egg = document.createElement("img");
+  const egg = document.createElement("div");
   egg.classList.add("egg");
-  egg.src = "/assets/egg_noBG.png";
-  egg.style.left = `${Math.random() * (gameContainer.clientWidth - 50)}px`;
-  egg.style.top = "0px";
+  egg.style.left = `${Math.random() * (gameContainer.offsetWidth - 30)}px`;
   gameContainer.appendChild(egg);
+  console.log("Egg  created: ", egg);
   return egg;
 }
 
-function updateGame() {}
+// Update game state
+function updateGame() {
+  const eggs = document.querySelectorAll(".egg");
+  eggs.forEach((egg) => {
+    const top = parseInt(egg.style.top || "0");
+    egg.style.top = `${top + 5}px`;
 
-// var player;
-// var eggs = [];
-// var numberOfEggs = 15;
+    // Check for collision with basket
+    const eggRect = egg.getBoundingClientRect();
+    const basketRect = basket.getBoundingClientRect();
+    if (
+      eggRect.bottom >= basketRect.top &&
+      eggRect.left >= basketRect.left &&
+      eggRect.right <= basketRect.right
+    ) {
+      // Caught the egg
+      score += 1;
+      scoreElement.textContent = score;
+      egg.remove();
+    } else if (top > gameContainer.offsetHeight) {
+      // Missed the egg
+      egg.remove();
+      lives -= 1;
+      livesContainer.removeChild(livesContainer.lastElementChild);
+      if (lives === 0) {
+        endGame();
+      }
+    }
+  });
+}
 
-// function Player() {
-//   this.gameOver = false;
-//   this.score = 0;
-//   this.eggsCollected = 0;
-//   this.lives = 3;
-//   this.playerWidth = 150;
-//   this.playerHeight = 90;
-//   this.playerSpeed = 20;
-//   this.x = canvas.width / 2;
-//   this.y = canvas.height - this.playerHeight - 10;
-//   this.playerImage = new Image();
-//   this.playerImage.src = "/assets/basket_noBG.png";
+// End the game
+function endGame() {
+  clearInterval(gameInterval);
+  alert("Game Over!");
+  // Redirect to game over scene or reset the game
+}
 
-//   this.draw = function () {
-//     ctx.drawImage(
-//       this.playerImage,
-//       this.x,
-//       this.y,
-//       this.playerWidth,
-//       this.playerHeight
-//     );
-//   };
+// Start the game
+function startGame() {
+  gameInterval = setInterval(() => {
+    createEgg();
+    updateGame();
+  }, 500);
+}
 
-//   this.move = function (direction) {
-//     if (direction === "left" && this.x > 0) {
-//       this.x -= this.playerSpeed;
-//     } else if (
-//       direction === "right" &&
-//       this.x < canvas.width - this.playerWidth
-//     ) {
-//       this.x += this.playerSpeed;
-//     }
-//   };
-// }
-
-// function Egg() {
-//   this.x = Math.random() * (canvas.width - 50);
-//   this.y = Math.random() * (canvas.height - 50);
-//   this.width = 50;
-//   this.height = 50;
-//   this.image = new Image();
-//   this.image.src = "/assets/egg_noBG.png";
-
-//   this.draw = function () {
-//     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-//   };
-
-//   this.checkCollision = function (player) {
-//     return (
-//       player.x < this.x + this.width &&
-//       player.x + player.playerWidth > this.x &&
-//       player.y < this.y + this.height &&
-//       player.y + player.playerHeight > this.y
-//     );
-//   };
-
-//   this.resetPosition = function () {
-//     this.x = Math.random() * (canvas.width - this.width);
-//     this.y = Math.random() * (canvas.height - this.height);
-//   };
-
-//   this.fall = function () {
-//     this.y += 5; // Fall speed
-//     if (this.y > canvas.height) {
-//       this.resetPosition();
-//     }
-//   };
-
-//   this.checkIfCaught = function () {
-//     if (this.y + this.height > canvas.height) {
-//       this.resetPosition();
-//       player.lives--;
-//       if (player.lives <= 0) {
-//         player.gameOver = true;
-//       }
-//     }
-//   };
-
-//   window.addEventListener("keydown", function (event) {
-//     if (event.key === "ArrowLeft") {
-//       player.move("left");
-//     } else if (event.key === "ArrowRight") {
-//       player.move("right");
-//     }
-//   });
-
-//   main();
-
-//   function main() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     player.draw();
-//     for (let i = 0; i < eggs.length; i++) {
-//       eggs[i].draw();
-//       eggs[i].fall();
-//       if (eggs[i].checkCollision(player)) {
-//         player.score++;
-//         player.eggsCollected++;
-//         eggs[i].resetPosition();
-//       }
-//       eggs[i].checkIfCaught();
-//     }
-
-//     if (player.gameOver) {
-//       alert("Game Over! Your score: " + player.score);
-//       window.location.href = "index.html";
-//     } else {
-//       requestAnimationFrame(main);
-//     }
-//   }
-
-//   function startGame() {
-//     player = new Player();
-//     for (let i = 0; i < numberOfEggs; i++) {
-//       eggs.push(new Egg());
-//     }
-//     main();
-//   }
-
-//   startGame();
-
-//   function updateGame() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     player.draw();
-//     for (let i = 0; i < eggs.length; i++) {
-//       eggs[i].draw();
-//       eggs[i].fall();
-//       if (eggs[i].checkCollision(player)) {
-//         player.score++;
-//         player.eggsCollected++;
-//         eggs[i].resetPosition();
-//       }
-//       eggs[i].checkIfCaught();
-//     }
-
-//     if (player.gameOver) {
-//       alert("Game Over! Your score: " + player.score);
-//       window.location.href = "index.html";
-//     } else {
-//       requestAnimationFrame(updateGame);
-//     }
-//   }
-// }
+startGame();
